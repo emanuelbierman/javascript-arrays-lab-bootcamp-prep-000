7c43822df4af6a43144b251bf7d1521b51109cc7
Emanuel Bierman <emanuel.bierman@gmail.com>
2017-11-02 16:47:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten() {
+  
+}
+
+function destructivelyPrependKitten() {
+
+}
+
+function destructivelyRemoveLastKitten() {
+  
+}
+
+function destructivelyRemoveFirstKitten() {
+  
+}
